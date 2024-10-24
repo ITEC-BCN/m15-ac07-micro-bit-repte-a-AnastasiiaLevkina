7e2e6bde5690ee9d08f7ce38ffc0737bab9fb900
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-24 17:44:19 +0200

    Añadir juego contra máquina

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let randomIcon = 0
+let randomIcon2 = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
@@ -34,6 +35,36 @@ input.onGesture(Gesture.Shake, function () {
             # . # # .
             . # . . #
             `)
+    }
+    basic.pause(1000)
+    randomIcon2 = randint(1, 3)
+    if (randomIcon == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (randomIcon == 2) {
+        basic.showLeds(`
+            . . # # .
+            . # # # #
+            # # # # #
+            # # # # .
+            . # # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . . #
+            # . # # .
+            . # # . .
+            # . # # .
+            . # . . #
+            `)
+    }
+    if (randomIcon == 1 && randomIcon2 == 3 || (randomIcon == 2 && randomIcon2 == 1 || randomIcon == 3 && randomIcon2 == 2)) {
+    	
     }
 })
 input.onButtonPressed(Button.AB, function () {
