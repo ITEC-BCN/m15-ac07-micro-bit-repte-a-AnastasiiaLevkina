709b02ae50aac0fb96a709a7f6bf18cc4d3712a2
Anastasiia Lekvina <146827165+AnastasiiaLevkina@users.noreply.github.com>
2024-10-28 15:33:20 +0100

    Terminado.

--- main.py
+++ main.py
@@ -1,12 +1,125 @@
-randomIcon = 0
+player_choice = 0
+machine_choice = 0
+random_choice_var = 0
+
+def on_button_pressed_a():
+    global player_choice
+    player_choice = 1
+    basic.show_leds("""
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        """)
+    machine_turn()
+    mostrar_resultat()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def machine_turn():
+    global machine_choice
+    basic.pause(1000)
+    machine_choice = random_choice()
+    if machine_choice == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif machine_choice == 2:
+        basic.show_leds("""
+            . . # # .
+            . # # # #
+            # # # # #
+            # # # # .
+            . # # . .
+            """)
+    else:
+        basic.show_leds("""
+            . # . . #
+            # . # # .
+            . # # . .
+            # . # # .
+            . # . . #
+            """)
+def random_choice():
+    global random_choice_var
+    random_choice_var = randint(1, 3)
+    return random_choice_var
 
 def on_gesture_shake():
-    global randomIcon
-    randomIcon = randint(1, 3)
-    if randomIcon == 1:
-        pass
-    elif randomIcon == 2:
-        pass
+    global player_choice
+    player_choice = random_choice()
+    if player_choice == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif player_choice == 2:
+        basic.show_leds("""
+            . . # # .
+            . # # # #
+            # # # # #
+            # # # # .
+            . # # . .
+            """)
     else:
-        pass
+        basic.show_leds("""
+            . # . . #
+            # . # # .
+            . # # . .
+            # . # # .
+            . # . . #
+            """)
+    machine_turn()
+    mostrar_resultat()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def on_button_pressed_ab():
+    global player_choice
+    player_choice = 3
+    basic.show_leds("""
+        . # . . #
+        # . # # .
+        . # # . .
+        # . # # .
+        . # . . #
+        """)
+    machine_turn()
+    mostrar_resultat()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global player_choice
+    player_choice = 2
+    basic.show_leds("""
+        . . # # .
+        . # # # #
+        # # # # #
+        # # # # .
+        . # # . .
+        """)
+    machine_turn()
+    mostrar_resultat()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def mostrar_resultat():
+    if machine_choice == 1 and player_choice == 2:
+        basic.show_string("You win!")
+    elif machine_choice == 2 and player_choice == 3:
+        basic.show_string("You win!")
+    elif machine_choice == 3 and player_choice == 1:
+        basic.show_string("You win!")
+    elif player_choice == 1 and machine_choice == 2:
+        basic.show_string("You lose!")
+    elif player_choice == 2 and machine_choice == 3:
+        basic.show_string("You lose!")
+    elif player_choice == 3 and machine_choice == 1:
+        basic.show_string("You lose!")
+    else:
+        basic.show_string("It's an even!")
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-let randomIcon = 0
-let randomIcon2 = 0
-input.onButtonPressed(Button.A, function () {
+let player_choice = 0
+let machine_choice = 0
+let random_choice_var = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    player_choice = 1
     basic.showLeds(`
         . . . . .
         . # # # .
@@ -8,37 +11,14 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . . . . .
         `)
+    machine_turn()
+    mostrar_resultat()
 })
-input.onGesture(Gesture.Shake, function () {
-    randomIcon = randint(1, 3)
-    if (randomIcon == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
-    } else if (randomIcon == 2) {
-        basic.showLeds(`
-            . . # # .
-            . # # # #
-            # # # # #
-            # # # # .
-            . # # . .
-            `)
-    } else {
-        basic.showLeds(`
-            . # . . #
-            # . # # .
-            . # # . .
-            # . # # .
-            . # . . #
-            `)
-    }
+function machine_turn() {
+    
     basic.pause(1000)
-    randomIcon2 = randint(1, 3)
-    if (randomIcon == 1) {
+    machine_choice = random_choice()
+    if (machine_choice == 1) {
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -46,7 +26,7 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
-    } else if (randomIcon == 2) {
+    } else if (machine_choice == 2) {
         basic.showLeds(`
             . . # # .
             . # # # #
@@ -63,11 +43,50 @@ input.onGesture(Gesture.Shake, function () {
             . # . . #
             `)
     }
-    if (randomIcon == 1 && randomIcon2 == 3 || (randomIcon == 2 && randomIcon2 == 1 || randomIcon == 3 && randomIcon2 == 2)) {
-    	
+    
+}
+
+function random_choice(): number {
+    
+    random_choice_var = randint(1, 3)
+    return random_choice_var
+}
+
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    player_choice = random_choice()
+    if (player_choice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (player_choice == 2) {
+        basic.showLeds(`
+            . . # # .
+            . # # # #
+            # # # # #
+            # # # # .
+            . # # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . . #
+            # . # # .
+            . # # . .
+            # . # # .
+            . # . . #
+            `)
     }
+    
+    machine_turn()
+    mostrar_resultat()
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    player_choice = 3
     basic.showLeds(`
         . # . . #
         # . # # .
@@ -75,8 +94,12 @@ input.onButtonPressed(Button.AB, function () {
         # . # # .
         . # . . #
         `)
+    machine_turn()
+    mostrar_resultat()
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    player_choice = 2
     basic.showLeds(`
         . . # # .
         . # # # #
@@ -84,4 +107,25 @@ input.onButtonPressed(Button.B, function () {
         # # # # .
         . # # . .
         `)
+    machine_turn()
+    mostrar_resultat()
 })
+function mostrar_resultat() {
+    if (machine_choice == 1 && player_choice == 2) {
+        basic.showString("You win!")
+    } else if (machine_choice == 2 && player_choice == 3) {
+        basic.showString("You win!")
+    } else if (machine_choice == 3 && player_choice == 1) {
+        basic.showString("You win!")
+    } else if (player_choice == 1 && machine_choice == 2) {
+        basic.showString("You lose!")
+    } else if (player_choice == 2 && machine_choice == 3) {
+        basic.showString("You lose!")
+    } else if (player_choice == 3 && machine_choice == 1) {
+        basic.showString("You lose!")
+    } else {
+        basic.showString("It's an even!")
+    }
+    
+}
+
